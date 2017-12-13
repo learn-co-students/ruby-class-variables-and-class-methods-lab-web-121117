b6e2899aefec4c8b3d2260fb2fccc0a43b8e5678
krandles <krandles@gmail.com>
2017-12-13 11:41:45 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -41,11 +41,7 @@ class Song
   def self.artist_count
     artist_hash = {}
     @@artists.each do |artist|
-      if !artist_hash[artist]
-        artist_hash[artist] = 1
-      else
-        artist_hash[artist] += 1
-      end
+      !artist_hash[artist] ? artist_hash[artist] = 1 : artist_hash[artist] += 1
     end
     artist_hash
   end
